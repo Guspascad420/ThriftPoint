cf7657f7d604e10642c07da7b82dfabba3b419f1
Guspascad420 <Paacad@gmail.com>
2023-11-03 12:16:52 +0700

    Updated LaunchedEffect block functions

--- app/src/main/java/com/example/thriftpoint/ui/screens/CartScreen.kt
+++ app/src/main/java/com/example/thriftpoint/ui/screens/CartScreen.kt
@@ -27,6 +27,7 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -38,8 +39,10 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
@@ -58,97 +61,117 @@ import com.example.thriftpoint.ui.theme.urbanist
 import com.example.thriftpoint.utils.NavRoute
 import com.example.thriftpoint.viewmodels.MainViewModel
 import com.example.thriftpoint.viewmodels.ProductViewModel
+import kotlinx.coroutines.delay
+import java.text.DecimalFormat
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun CartScreen(navController: NavHostController) {
+fun CartScreen(productViewModel: ProductViewModel, navController: NavHostController) {
     val mainViewModel = hiltViewModel<MainViewModel>()
-    val productViewModel = hiltViewModel<ProductViewModel>()
     mainViewModel.selectedItem = "Bag"
+    val productsInCartState = productViewModel.productsInCartState.collectAsState()
+    val productsInWishlistState = productViewModel.productsInWishlistState.collectAsState()
     var count by remember { mutableIntStateOf(1) }
     var totalPrice by remember { mutableIntStateOf(0) }
+    val formatter = DecimalFormat("#,###")
+    var isLoading by remember { mutableStateOf(true) }
 
     LaunchedEffect(Unit) {
-        productViewModel.getAllProductsInCart()
-        productViewModel.getWishlist()
+        if (productViewModel.productsInWishlist.isEmpty() || productViewModel.productsInCart.isEmpty()) {
+            productViewModel.getAllProductsInCart()
+            productViewModel.getWishlist()
+            delay(2000)
+            productsInCartState.value.data?.let { response ->
+                if (response.data != productViewModel.productsInCart)
+                    productViewModel.productsInCart.addAll(response.data)
+            }
+            productsInWishlistState.value.data?.let { response ->
+                if (response.data != productViewModel.productsInWishlist)
+                    productViewModel.productsInWishlist.addAll(response.data)
+            }
+        }
+        isLoading = false
     }
 
     Scaffold(
         topBar = { CartTopBar(navController) },
         bottomBar = { BottomBar(mainViewModel, navController) }
     ) {
+        if (!isLoading)
         Column(Modifier.padding(it)) {
             Spacer(Modifier.height(25.dp))
-            if (productViewModel.productsInCart.isNotEmpty()) {
-                productViewModel.productsInCart.forEach { product ->
-                    Card(
-                        Modifier
-                            .fillMaxWidth()
-                            .padding(horizontal = 20.dp),
-                        shape = RoundedCornerShape(14.dp),
-                        colors = CardDefaults.cardColors(Color.White),
-                        elevation = CardDefaults.cardElevation(10.dp)
-                    ) {
-                        Row(Modifier.padding(12.dp)) {
-                            AsyncImage(
-                                HttpEndpoint.IMG_BASE_URL + product.imageRes ,
-                                null,
-                                modifier = Modifier
-                                    .size(95.dp)
-                                    .clip(RoundedCornerShape(14.dp))
+            totalPrice = productViewModel.productsInCart.sumOf { product -> product.price }
+            productViewModel.productsInCart.forEach { product ->
+                Card(
+                    Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = 20.dp),
+                    shape = RoundedCornerShape(14.dp),
+                    colors = CardDefaults.cardColors(Color.White),
+                    elevation = CardDefaults.cardElevation(10.dp)
+                ) {
+                    Row(Modifier.padding(12.dp)) {
+                        AsyncImage(
+                            HttpEndpoint.IMG_BASE_URL + product.imageRes ,
+                            null,
+                            modifier = Modifier
+                                .size(95.dp)
+                                .clip(RoundedCornerShape(14.dp))
+                        )
+                        Column(Modifier.padding(start = 15.dp)) {
+                            Text(
+                                product.name, fontFamily = urbanist, fontSize = 16.sp,
+                                fontWeight = FontWeight.Medium
                             )
-                            Column(Modifier.padding(start = 15.dp)) {
-                                Text(
-                                    product.name, fontFamily = urbanist, fontSize = 16.sp,
-                                    fontWeight = FontWeight.Medium
-                                )
-                                Text("${product.price}", fontFamily = urbanist, fontSize = 20.sp)
-                                Spacer(Modifier.height(40.dp))
-                                Row(Modifier.fillMaxWidth(), Arrangement.SpaceBetween) {
-                                    Row(verticalAlignment = Alignment.CenterVertically) {
-                                        Surface(
-                                            Modifier.size(35.dp), shape = CircleShape,
-                                            border = BorderStroke(1.dp, Color(0xFFDEDEDE)),
-                                            color = MaterialTheme.colorScheme.background
-                                        ) {
-                                            IconButton(onClick = {
-                                                if (count != 0) {
-                                                    count -= 1
-                                                }
-                                            }) {
-                                                Icon(Icons.Default.KeyboardArrowDown, null)
+                            Text("Rp ${formatter.format(product.price)}",
+                                fontFamily = urbanist, fontSize = 20.sp)
+                            Spacer(Modifier.height(40.dp))
+                            Row(Modifier.fillMaxWidth(), Arrangement.SpaceBetween) {
+                                Row(verticalAlignment = Alignment.CenterVertically) {
+                                    Surface(
+                                        Modifier.size(35.dp), shape = CircleShape,
+                                        border = BorderStroke(1.dp, Color(0xFFDEDEDE)),
+                                        color = MaterialTheme.colorScheme.background
+                                    ) {
+                                        IconButton(onClick = {
+                                            if (count != 0) {
+                                                count -= 1
                                             }
-                                        }
-                                        Text("$count", Modifier.padding(horizontal = 5.dp))
-                                        Surface(
-                                            Modifier.size(35.dp), shape = CircleShape,
-                                            border = BorderStroke(1.dp, Color(0xFFDEDEDE)),
-                                            color = MaterialTheme.colorScheme.background
-                                        ) {
-                                            IconButton(onClick = { count += 1 }) {
-                                                Icon(Icons.Default.KeyboardArrowUp, null)
-                                            }
+                                        }) {
+                                            Icon(Icons.Default.KeyboardArrowDown, null)
                                         }
                                     }
-                                    Row {
-                                        Surface(
-                                            Modifier.size(35.dp), shape = CircleShape,
-                                            border = BorderStroke(1.dp, Color(0xFFDEDEDE)),
-                                            color = MaterialTheme.colorScheme.background
-                                        ) {
-                                            IconButton(onClick = { /*TODO*/ }) {
-                                                Icon(Icons.Default.FavoriteBorder, null)
-                                            }
+                                    Text("$count", Modifier.padding(horizontal = 5.dp))
+                                    Surface(
+                                        Modifier.size(35.dp), shape = CircleShape,
+                                        border = BorderStroke(1.dp, Color(0xFFDEDEDE)),
+                                        color = MaterialTheme.colorScheme.background
+                                    ) {
+                                        IconButton(onClick = { count += 1 }) {
+                                            Icon(Icons.Default.KeyboardArrowUp, null)
                                         }
-                                        Spacer(Modifier.width(5.dp))
-                                        Surface(
-                                            Modifier.size(35.dp), shape = CircleShape,
-                                            border = BorderStroke(1.dp, Color(0xFFDEDEDE)),
-                                            color = MaterialTheme.colorScheme.background
-                                        ) {
-                                            IconButton(onClick = { productViewModel.productsInCart.remove(product) }) {
-                                                Icon(Icons.Default.Delete, null)
-                                            }
+                                    }
+                                }
+                                Row {
+                                    Surface(
+                                        Modifier.size(35.dp), shape = CircleShape,
+                                        border = BorderStroke(1.dp, Color(0xFFDEDEDE)),
+                                        color = MaterialTheme.colorScheme.background
+                                    ) {
+                                        IconButton(onClick = { /*TODO*/ }) {
+                                            Icon(Icons.Default.FavoriteBorder, null)
+                                        }
+                                    }
+                                    Spacer(Modifier.width(5.dp))
+                                    Surface(
+                                        Modifier.size(35.dp), shape = CircleShape,
+                                        border = BorderStroke(1.dp, Color(0xFFDEDEDE)),
+                                        color = MaterialTheme.colorScheme.background
+                                    ) {
+                                        IconButton(onClick = {
+                                            productViewModel.removeProductFromCart(product)
+                                        }) {
+                                            Icon(Icons.Default.Delete, null)
                                         }
                                     }
                                 }
@@ -173,45 +196,48 @@ fun CartScreen(navController: NavHostController) {
                         fontSize = 16.sp, color = Tosca40
                     )
                 }
-                LazyRow {
-                    items(productViewModel.productsInWishlist.toList()) { product ->
-                        Card(
-                            Modifier.padding(start = 20.dp),
-                            shape = RoundedCornerShape(14.dp),
-                            colors = CardDefaults.cardColors(Color.White),
-                            elevation = CardDefaults.cardElevation(10.dp)
-                        ) {
-                            Row(Modifier.padding(12.dp)) {
-                                AsyncImage(
-                                    HttpEndpoint.IMG_BASE_URL + product.imageRes ,
-                                    null,
-                                    modifier = Modifier
-                                        .size(95.dp)
-                                        .clip(RoundedCornerShape(14.dp))
+            }
+            LazyRow {
+                items(productViewModel.productsInWishlist) { product ->
+                    Card(
+                        Modifier.padding(start = 20.dp),
+                        shape = RoundedCornerShape(14.dp),
+                        colors = CardDefaults.cardColors(Color.White),
+                        elevation = CardDefaults.cardElevation(10.dp)
+                    ) {
+                        Row(Modifier.padding(12.dp)) {
+                            AsyncImage(
+                                HttpEndpoint.IMG_BASE_URL + product.imageRes ,
+                                null,
+                                modifier = Modifier
+                                    .size(95.dp)
+                                    .clip(RoundedCornerShape(14.dp))
+                            )
+                            Column(Modifier.padding(start = 15.dp)) {
+                                Text(
+                                    product.name, fontFamily = urbanist, fontSize = 16.sp,
+                                    fontWeight = FontWeight.Medium
                                 )
-                                Column(Modifier.padding(start = 15.dp)) {
-                                    Text(
-                                        product.name, fontFamily = urbanist, fontSize = 16.sp,
-                                        fontWeight = FontWeight.Medium
-                                    )
-                                    Text("${product.price}", fontFamily = urbanist, fontSize = 20.sp)
-                                    Spacer(Modifier.height(40.dp))
-                                    Row {
-                                        OutlinedButton(
-                                            onClick = { /*TODO*/ },
-                                            border = BorderStroke(2.dp, Tosca40)
-                                        ) {
-                                            Text("+ Keranjang", Modifier.padding(10.dp, 4.dp))
-                                        }
-                                        Spacer(Modifier.width(10.dp))
-                                        Surface(
-                                            Modifier.size(35.dp), shape = CircleShape,
-                                            border = BorderStroke(1.dp, Color(0xFFDEDEDE)),
-                                            color = MaterialTheme.colorScheme.background
-                                        ) {
-                                            IconButton(onClick = { productViewModel.productsInCart.remove(product) }) {
-                                                Icon(Icons.Default.Delete, null)
-                                            }
+                                Text("Rp ${formatter.format(product.price)}",
+                                    fontFamily = urbanist, fontSize = 20.sp)
+                                Spacer(Modifier.height(40.dp))
+                                Row {
+                                    OutlinedButton(
+                                        onClick = { /*TODO*/ },
+                                        border = BorderStroke(2.dp, Tosca40)
+                                    ) {
+                                        Text("+ Keranjang", Modifier.padding(10.dp, 4.dp))
+                                    }
+                                    Spacer(Modifier.width(10.dp))
+                                    Surface(
+                                        Modifier.size(35.dp), shape = CircleShape,
+                                        border = BorderStroke(1.dp, Color(0xFFDEDEDE)),
+                                        color = MaterialTheme.colorScheme.background
+                                    ) {
+                                        IconButton(onClick = {
+                                           productViewModel.removeProductFromCart(product)
+                                        }) {
+                                            Icon(Icons.Default.Delete, null)
                                         }
                                     }
                                 }
@@ -238,7 +264,7 @@ fun CartScreen(navController: NavHostController) {
                                 fontWeight = FontWeight.Medium
                             )
                             Text(
-                                "RP $totalPrice", fontFamily = urbanist,
+                                "Rp ${formatter.format(totalPrice)}", fontFamily = urbanist,
                                 fontWeight = FontWeight.Bold, color = Tosca40
                             )
                         }
@@ -257,6 +283,9 @@ fun CartScreen(navController: NavHostController) {
                     }
                 }
             }
+        }
+        else Box(Modifier.fillMaxSize().padding(it), contentAlignment = Alignment.Center) {
+            CircularProgressIndicator()
         }
     }
 }
